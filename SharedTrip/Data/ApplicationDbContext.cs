1ad059e6458879b624cdbfca8d192647001989ad
Anton Jekov <antonjekov@gmail.com>
2020-10-19 10:40:06 +0200

    Shared Trip static files

---
+++ SharedTrip/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedTrip.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public static string ConnectionString = @"Server=.\SQLEXPRESS01;Database=SharedTrip;Integrated Security = True";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ConnectionString);
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
